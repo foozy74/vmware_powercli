7efaf4078fe969edd8a9a7034a2dd5532a73b1f8
foozy74 <j.mueller@thesolution.at>
2019-10-29 15:48:45 +0100

    Update change-storage-policy.ps1

--- storage/change-storage-policy.ps1
+++ storage/change-storage-policy.ps1
@@ -2,3 +2,7 @@
 Get-VMHost | Get-ScsiLun -LunType disk | Where {$_.MultipathPolicy -notlike "RoundRobin"}
 
 Get-VMHost | Get-ScsiLun -LunType disk | Where {$_.MultipathPolicy -notlike "RoundRobin"} | Where {$_.CapacityGB -ge 100}
+
+Get-VMHost | Get-ScsiLun -LunType disk | Where {$_.MultipathPolicy -notlike "RoundRobin"} | Where {$_.CapacityGB -ge 100} | Set-Scsilun -MultiPathPolicy RoundRobin
+
+
